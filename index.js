1285b49beeab43c7244e8cffd846c94a95570e32
Tomi <tominiiranen1@gmail.com>
2020-02-07 17:58:28 +0200

    tyyli

--- index.js
+++ index.js
@@ -6,17 +6,15 @@ const api = require('./api/api');
 const app = express();
 const port = 3001;
 
-
 app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.get('/', (req, res) => {
-    res.send(`hahah`);
+  res.send(`hahah`);
 });
 
-app.use('/api', api)
+app.use('/api', api);
 
 app.listen(port, () => console.log(`App running on http://localhost:${port}`));
