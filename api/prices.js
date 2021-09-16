e907c0c201b399ccd4850554568e500de3bfd9e3
Tomi Niiranen <tominiiranen@MacBook-Pro>
2021-09-17 00:54:13 +0300

    giganttiin lisätty ei haku tulosta varmistus

--- api/prices.js
+++ api/prices.js
@@ -23,6 +23,14 @@ async function priceFromGigantti(ean) {
     await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36")
     await page.goto(`https://www.gigantti.fi/search?SearchTerm=${ean}`);
 
+    const noSearchResult = await page.evaluateHandle(() => {
+      return document.querySelector('.no-search-result')
+    });
+
+    if (noSearchResult._remoteObject.value === undefined) {
+      return JSON.stringify(result);
+    }
+
     const name = await page.evaluateHandle(() => {
       return document.querySelector('.product-title').innerText;
     });
